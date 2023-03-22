71163caa73de9494ae3cf5ed90f00d2b14728b76
rajibrt <rajibrt@gmail.com>
2023-03-22 18:16:23 +0600

    mongodb added

--- index.js
+++ index.js
@@ -2,26 +2,45 @@ const express = require('express')
 const app = express()
 const products = require('./products.json')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb')
 const port = process.env.PORT || 4000
 
 app.use(cors())
 
+const uri =
+  'mongodb+srv://raishahost:JEAb2CJ6ksRA7p2N@cluster0.nz65omc.mongodb.net/?retryWrites=true&w=majority'
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+})
+async function run() {
+  try {
+    const productsCollection = client.db('eshop').collection('products')
+    const product = { name: 'OnePlus 10 Pro', brand: 'OnePlus' }
+    const result = await productsCollection.insertOne(product)
+    console.log(result)
+  } finally {
+  }
+}
+
+run().catch((err) => console.log(err))
+
 app.get('/', (req, res) => {
-    res.send('Hello from node js')
+  res.send('Hello from node js')
 })
 
 app.get('/products', (req, res) => {
-    res.send(products)
+  res.send(products)
 })
 
 app.get('/product/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    console.log(id)
-    const product = products.find(product => product._id === id) || {};
-    res.send(product)
+  const id = parseInt(req.params.id)
+  console.log(id)
+  const product = products.find((product) => product._id === id) || {}
+  res.send(product)
 })
 
 app.listen(port, () => {
-    console.log(`Server is running at: ${port}`)
+  console.log(`Server is running at: ${port}`)
 })
-
