5c422e2e8f39826780595e5ca1d56ee576b9153f
rajibrt <rajibrt@gmail.com>
2023-03-25 11:33:30 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const products = require('./products.json')
 const cors = require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb')
 const port = process.env.PORT || 4000
+require('dotenv').config()
 
 app.use(cors())
 
-const uri =
-  'mongodb+srv://raishahost:JEAb2CJ6ksRA7p2N@cluster0.nz65omc.mongodb.net/?retryWrites=true&w=majority'
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nz65omc.mongodb.net/?retryWrites=true&w=majority`
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
